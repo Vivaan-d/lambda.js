c92b19067ab9a21287cc87420d34f7b924f45f7d
Vivaan-d <75510334+Vivaan-d@users.noreply.github.com>
2025-04-16 21:52:07 -0400

    Update and rename g.js to lambda.js

--- 1.0.0/lambda.js
+++ 1.0.0/lambda.js
@@ -1,23 +1,23 @@
 /*
-G.JS
-Initial Release
-V1.0.0
+lambda.JS
+Reintroduction
+V1.0.1
 */
-const g = {};
-g.v = {/* variables */};
-g.element=(selector, command, commandValue)=>{
+const λ = {};
+λ.v = {/* variables */};
+λ.element=(selector, command, commandValue)=>{
     if (command == 'HTML'){document.querySelector(selector).innerHTML = commandValue}
     else if (command == 'html'){document.querySelector(selector).innerHTML = commandValue}
 }
-g.getElement = (selector, get) => {
+λ.getElement = (selector, get) => {
     if (get == 'HTML'){return document.querySelector(selector).innerHTML} 
     else if (get == 'html'){return document.querySelector(selector).innerHTML} else {return document.querySelector(selector)}}
-g.replaceHTML = (selector, ...replaceparams) => {
+λ.replaceHTML = (selector, ...replaceparams) => {
     return document.querySelector(selector).innerHTML.replace(replaceparams)
 }
-g.window = {
+λ.window = {
     open: (...params)=>{window.open(params)}
 }
-Object.freeze(g)
-Object.freeze(g.window)
-v = g.v;
+Object.freeze(λ)
+Object.freeze(λ.window)
+v = λ.v;
